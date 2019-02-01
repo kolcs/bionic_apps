68920d070710e3ef03b2b8f457215e8e07229135
Köllőd Csaba <kollod.csaba@itk.ppke.hu>
2019-02-01 21:53:02 +0100

    firs commit

---
+++ BCISystem.py
@@ -0,0 +1,51 @@
+from config import *
+from preprocess import DBProcessor
+from BCI_AI import NeuralNetwork
+
+
+# TODO: build NN
+
+class BCISystem(object):
+
+    def __init__(self, base_dir="", window=0.0625, step=0.03125, fs=160): # TODO: check base dir
+        """
+        Constructor for BCI system
+
+        :param base_dir: base directory of databases
+            in case of offline process - source folder of database
+            in case of online process - where to save database
+        :param window: eeg window length in seconds
+        :param step: window shift parameter in seconds
+        :param fs: sampling fequency
+        """
+        self._base_dir = base_dir
+        self._window = window  # in seconds
+        self._step = step  # in seconds
+        self._fs = fs
+
+    def offline_processing(self):
+        """
+        This is the main function which starts the offline processing
+        """
+        directory = self._base_dir + DIR_TF_RECORDS
+
+        filenames = dict()
+        filenames[TRAIN] = DBProcessor.get_filenames_in(directory + DIR_TRAIN, ext=F_EXT_TF_RECORD)
+        filenames[VALIDATION] = DBProcessor.get_filenames_in(directory + DIR_VALIDATION, ext=F_EXT_TF_RECORD)
+        filenames[TEST] = DBProcessor.get_filenames_in(directory + DIR_TEST, ext=F_EXT_TF_RECORD)
+
+        nn = NeuralNetwork()
+        nn.offline_training(filenames)
+
+    def online_processing(self):
+        raise NotImplementedError("Online processing is not implemented...")
+
+
+if __name__ == '__main__':
+    base_dir = "D:/BCI_stuff/databases/"  # MTA TTK
+    # base_dir = 'D:/Csabi/'  # Home
+    # base_dir = "D:/Users/Csabi/data"  # ITK
+    # base_dir = "/home/csabi/databases/"  # linux
+
+    bci = BCISystem(base_dir)
+    bci.offline_processing()
---
+++ config.py
@@ -0,0 +1,117 @@
+"""
+Configuration for databases
+"""
+# Record types:
+IMAGINED_MOVEMENT = "imagined"
+REAL_MOVEMENT = "real"
+BASELINE = 'baseline'
+
+# Task types:
+EYE_OPEN = 'eye open'
+EYE_CLOSED = 'eye closed'
+LEFT_HAND = 'left hand'
+RIGHT_HAND = 'right hand'
+BOTH_HANDS = 'both hands'
+LEFT_LEG = 'left_leg'
+RIGHT_LEG = 'right_leg'
+BOTH_LEGS = 'both legs'
+REST = 'rest'
+
+SUBJECT = 'subject'
+
+DATA_START_AFTER = 1  # sec
+DATA_END_BEFORE = 0.5  # sec
+
+RECORD_TO_NUM = {key: value for value, key in enumerate([IMAGINED_MOVEMENT, REAL_MOVEMENT, BASELINE])}
+TASK_TO_NUM = {key: value for value, key in enumerate(
+    [EYE_OPEN, EYE_CLOSED, LEFT_HAND, RIGHT_HAND, BOTH_HANDS, LEFT_LEG, RIGHT_LEG, BOTH_LEGS, REST])}
+ONE_HOT_LIST = [RECORD_TO_NUM, TASK_TO_NUM]
+
+DATA = 'data'
+DATA_SHAPE = 'data shape'
+RECORD_TYPE_LABEL = 'record type label'
+TASK_LABEL = 'task label'
+
+TRAIN = 'train'
+VALIDATION = 'validation'
+TEST = 'test'
+
+DIR_TRAIN = 'train/'
+DIR_VALIDATION = 'validation/'
+DIR_TEST = 'test/'
+DIR_TF_RECORDS = 'tfRecords/'
+
+F_EXT_TF_RECORD = '.tfrecord'
+
+CHANNEL_TRANSFORMATION = [35, 36, 37, 38, 39, 40, 41, 46, 47, 48, 49, 50, 51, 52, 57, 58, 59, 60, 61, 62, 63, 4, 5,
+                          6, 14, 15, 16, 17, 18, 23, 24, 25, 26, 27, 28, 29, 30, 31, 34, 42, 45, 53, 44, 54, 56, 64,
+                          67, 68, 69, 70, 71, 72, 73, 74, 75, 80, 81, 82, 83, 84, 92, 93, 94, 104]
+
+
+# # source:
+# list(range(35, 42)) + list(range(46, 53)) + list(range(57, 64)) + list(range(4, 7)) + list(
+# range(14, 19)) + list(range(23, 32)) + [34, 42, 45, 53, 44, 54, 56, 64] + list(range(67, 76)) + list(
+# range(80, 85)) + list(range(92, 95)) + [104]
+
+
+class Physionet:
+    DIR = "physionet.org/"
+    CHANNEL_NUM = 64
+    FS = 160
+
+    TASK_12 = {
+        'T0': REST,
+        'T1': LEFT_HAND,
+        'T2': RIGHT_HAND
+    }
+
+    TASK_34 = {
+        'T0': REST,
+        'T1': BOTH_HANDS,
+        'T2': BOTH_LEGS
+    }
+
+    TRIGGER_TYPE_CONVERTER = {
+        1: BASELINE,
+        2: BASELINE,
+        3: REAL_MOVEMENT,
+        4: IMAGINED_MOVEMENT,
+        5: REAL_MOVEMENT,
+        6: IMAGINED_MOVEMENT,
+        7: REAL_MOVEMENT,
+        8: IMAGINED_MOVEMENT,
+        9: REAL_MOVEMENT,
+        10: IMAGINED_MOVEMENT,
+        11: REAL_MOVEMENT,
+        12: IMAGINED_MOVEMENT,
+        13: REAL_MOVEMENT,
+        14: IMAGINED_MOVEMENT
+    }
+
+    TRIGGER_TASK_CONVERTER = {  # rec_num : {taskID: task}
+        1: {'T0': EYE_OPEN},
+        2: {'T0': EYE_CLOSED},
+        3: TASK_12,
+        4: TASK_12,
+        5: TASK_34,
+        6: TASK_34,
+        7: TASK_12,
+        8: TASK_12,
+        9: TASK_34,
+        10: TASK_34,
+        11: TASK_12,
+        12: TASK_12,
+        13: TASK_34,
+        14: TASK_34
+    }
+
+    MAX_DURATION = 4  # seconds --> creating strictly formatted data window
+
+    """
+       DROP_SUBJECTS:
+    
+       list of subjects, whos records are corrupted
+       89 - wrong baseline session(T0 with T1)
+       88, 92, 100 - wrong intervals (1,375, 5,125) and freq 128Hz instead of 160Hz
+       """
+    DROP_SUBJECTS = [89, 88, 92, 100]
