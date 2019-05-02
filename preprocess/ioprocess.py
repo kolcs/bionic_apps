f5af1f3984289ac3f38f9043e3ce0644e95ed883
Köllőd Csaba <kollod.csaba@itk.ppke.hu>
2019-05-02 13:08:30 +0200

    continue dev... freq missmatch, sort by subject, real-img

--- preprocess/ioprocess.py
+++ preprocess/ioprocess.py
@@ -34,6 +34,7 @@ class OfflineEpochCreator:
         """
         self._data_path = self._base_dir + db_type.DIR
         self._db_type = db_type
+        self._drop_subject = db_type.DROP_SUBJECTS
 
     def use_physionet(self):
         self._use_db(Physionet)
@@ -81,20 +82,21 @@ class OfflineEpochCreator:
     def _get_filenames(self):
         return self.get_filenames_in(self._data_path, self._db_ext)
 
-    def _open_raw_file(self, filename, preload=True):
+    def _open_raw_file(self, filename, preload=True, stim_channel='auto'):
         ext = filename.split('.')[-1]
 
         switcher = {
             'edf': mne.io.read_raw_edf,
-            'eeg': mne.io.read_raw_brainvision,
+            'eeg': mne.io.read_raw_brainvision,  # todo: check...
         }
         # Get the function from switcher dictionary
         mne_io_read_raw = switcher.get(ext, lambda: "nothing")
         # Execute the function
-        return mne_io_read_raw(filename, preload=preload)
+        return mne_io_read_raw(filename, preload=preload, stim_channel=stim_channel)
 
     def _get_concatenated_raw_file(self, filenames):
-        raw_list = [self._open_raw_file(file) for file in filenames]
+        raw_list = [self._open_raw_file(file) for file in filenames if
+                    self.get_subject_number(file) not in self._drop_subject]
         raw = mne.io.concatenate_raws(raw_list)
         return raw
 
@@ -110,15 +112,57 @@ class OfflineEpochCreator:
         # files = [ for i in rec_nums for fname in filenames]
         return files
 
+    @staticmethod
+    def _get_num_with_predefined_char(filename, char, required_num='required'):
+        """
+        Searches for number in filename
+
+        :param char: character to search for
+        :param required_num: string to print in error message
+        :return: number after given char
+        :raises FileNotFoundError if none numbers were found
+        """
+        import re
+        num_list = re.findall(r'.*' + char + '\d+', filename)
+        if not num_list:
+            raise FileNotFoundError(
+                "Can not give back {} number: filename '{}' does not contain '{}' character.".format(required_num,
+                                                                                                     filename,
+                                                                                                     char))
+        num_str = num_list[0]
+        num_ind = num_str.rfind(char) - len(num_str) + 1
+        return int(num_str[num_ind:])
+
+    @staticmethod
+    def get_record_number(filename):
+        """
+        Only works if record number is stored in filename:
+            *R<record num>*
+
+        :return: record number from filename
+        """
+        return OfflineEpochCreator._get_num_with_predefined_char(filename, 'R', "RECORD")
+
+    @staticmethod
+    def get_subject_number(filename):
+        """
+        Only works if subject number is stored in filename:
+            *S<record num>*
+
+        :return: record number from filename
+        """
+        return OfflineEpochCreator._get_num_with_predefined_char(filename, 'S', "SUBJECT")
+
     def _create_epochs_from_db(self):
-        files = self._get_filenames()
+        filenames = self._get_filenames()
         # TODO: sort filenames by TRIGGER_TASK_LIST + subject
 
         # TODO: filter for real and img
         # TODO: frep missmatch...
         # TODO: drop wrong...
         for tasks in self._trigger_task_list:
-            files = self._find_filenames(tasks, files)
+            files = self._find_filenames(tasks, filenames)
+
             raw = self._get_concatenated_raw_file(files)
             events = mne.find_events(raw, shortest_event=0, stim_channel='STI 014')
             print(events)
@@ -126,6 +170,17 @@ class OfflineEpochCreator:
             #                    exclude='bads')
             # epochs = mne.Epochs(raw, events, event_id, tmin, tmax, proj=True, picks=picks,
             #                 baseline=None, preload=True)
+
+    def _create_annotated_db(self):
+        filenames = self._get_filenames()
+        for file in filenames:
+            rec_num = self.get_record_number(file)
+            subj_num = self.get_subject_number(file)
+
+            if subj_num in self._drop_subject:
+                continue
+
+
 
 
 if __name__ == '__main__':
--- trial.py
+++ trial.py
@@ -1,21 +1,22 @@
 import mne
 import numpy as np
+from config import *
 
 
 def use_mne_edf(filename):
-    raw = mne.io.read_raw_edf(filename, preload=False, stim_channel='auto')
+    raw = mne.io.read_raw_edf(filename, preload=False, stim_channel=None)
     # print(raw)
-    print(raw.info)
-    print(raw.find_edf_events())
-    raw = raw.load_data()
-    print(raw.find_edf_events())
-    data, time = raw[:,:]
-    print(data)
+    # print(raw.info)
+    # print(raw.find_edf_events())
+    # raw = raw.load_data()
+    # print(raw.find_edf_events())
+    # data, time = raw[:,:]
+    # print(data)
 
-    # n = raw.info['nchan']  # trigger channel!
-    # signal_labels = raw.info['ch_names']
-    # fs = raw.info['sfreq']
-    # print(n, signal_labels, fs)
+    n = raw.info['nchan']  # trigger channel!
+    signal_labels = raw.info['ch_names']
+    fs = raw.info['sfreq']
+    print(n, signal_labels, fs)
     # data, time = raw[:n, :]
     # print(np.shape(data))
     # events = mne.find_events(raw)
@@ -29,6 +30,53 @@ def use_mne_edf(filename):
     # print(np.shape(sigbufs))
 
 
+def open_raw_file(filename, preload=True, stim_channel='auto'):
+    ext = filename.split('.')[-1]
+
+    switcher = {
+        'edf': mne.io.read_raw_edf,
+        'eeg': mne.io.read_raw_brainvision,  # todo: check...
+    }
+    # Get the function from switcher dictionary
+    mne_io_read_raw = switcher.get(ext, lambda: "nothing")
+    # Execute the function
+    return mne_io_read_raw(filename, preload=preload, stim_channel=stim_channel)
+
+
+def _get_concatenated_raw_file(filenames):
+    raw_list = [open_raw_file(file) for file in filenames]
+    raw = mne.io.concatenate_raws(raw_list)
+    return raw
+
+
+def mne_epochs_and_save(files):
+    raw = _get_concatenated_raw_file(files)
+    events = mne.find_events(raw, shortest_event=0, stim_channel='STI 014')
+
+    event_id = {
+        REST: 1,
+        LEFT_HAND: 3,
+        RIGHT_HAND: 2
+    }
+    tmin = 0
+    tmax = 4
+    epochs = mne.Epochs(raw, events, event_id, tmin, tmax, proj=True,
+                        baseline=None, preload=True)
+    epochs.save('../tmp/data.fif', fmt='double')
+
+
+def open_epoch(filename):
+    epoch = mne.read_epochs(filename)
+    # epoch.plot(n_epochs=2, n_channels=65, block=True)
+    print(epoch.info)
+    print(epoch.events)
+    print(epoch[REST].events)
+    print(np.shape(epoch[REST].next()))
+
+
 if __name__ == '__main__':
-    file = "/home/csabi/databases/physionet.org/physiobank/database/eegmmidb/S001/S001R03.edf"
-    use_mne_edf(file)
+    files = ["/home/csabi/databases/physionet.org/physiobank/database/eegmmidb/S001/S001R03.edf",
+             "/home/csabi/databases/physionet.org/physiobank/database/eegmmidb/S001/S001R04.edf"]
+    # use_mne_edf(file[0])
+    mne_epochs_and_save(files)
+    open_epoch('../tmp/data.fif')
