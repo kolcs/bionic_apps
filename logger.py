d2e87cf6ffc43bdca11a10443526a870c4c40ef0
Köllőd Csaba <kollod.csaba@itk.ppke.hu>
2019-10-31 12:32:23 +0100

    Logger updated with docstring.

--- logger.py
+++ logger.py
@@ -5,6 +5,25 @@ from preprocess import make_dir
 
 
 def setup_logger(logger_name, log_file='', log_dir='log/', level=logging.INFO, log_to_stream=False):
+    """Logger creation function.
+
+    This function creates a logger, which has a separated log file, where it will append the logs.
+
+    Parameters
+    ----------
+    logger_name : str
+        Name of logger.
+    log_file : str
+        This string will be added to the filename. By default the filename contains
+        the creation time and the .log extension
+    log_dir : str
+        The path where to save the .log files.
+    level : logging const
+        The level of log.
+    log_to_stream : bool
+        Log info to the stream also.
+
+    """
     make_dir(log_dir)
     logger = logging.getLogger(logger_name)
     formatter = logging.Formatter('%(asctime)s : %(name)s %(levelname)s: %(message)s')
@@ -22,5 +41,17 @@ def setup_logger(logger_name, log_file='', log_dir='log/', level=logging.INFO, l
 
 
 def log_info(logger_name, msg):
+    """Logging info.
+
+    If the logger was created before it will log the given message into it.
+
+    Parameters
+    ----------
+    logger_name : str
+        Logger name, which connects the info to the file.
+    msg : str
+        Log message
+
+    """
     logger = logging.getLogger(logger_name)
     logger.info(msg)
