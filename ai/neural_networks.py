7390140b60ab1e13ffb8091d290cb55e81c13dc5
Köllőd Csaba <kollod.csaba@itk.ppke.hu>
2020-08-18 10:50:50 +0200

    Dev: NN
    Graph connection is ok.

--- ai/neural_networks.py
+++ ai/neural_networks.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 
+import tensorflow as tf
 from tensorflow import keras
+
 from ai.classifier import ClassifierInterface
 
 
@@ -41,35 +43,35 @@ class SimpleFeedForwardNetwork(BaseNetwork):
 class VGG19(ClassifierInterface):
 
     def __init__(self, classes, input_shape):
+        input_tensor = keras.layers.Input(shape=input_shape)
+        x = input_tensor
+
+        if len(input_shape) == 2:
+            x = keras.layers.Lambda(lambda tens: tf.expand_dims(tens, axis=-1))(input_tensor)
+            x = keras.layers.Lambda(lambda tens: tf.image.grayscale_to_rgb(tens))(x)
+
         base_model = keras.applications.VGG19(
             include_top=False,
             weights="imagenet",
-            input_tensor=None,
+            input_tensor=x,
             input_shape=None,
             pooling=None,
         )
-        # change input layer
-        base_model.summary()
-        base_model.layers.pop(0)
-        base_model.summary()
-        input_layer = keras.layers.Input(shape=input_shape)
 
-        # todo: expand to gray + expand dims --> 2d pic to rgb pic
-        # tf.image.grayscale_to_rgb(images, name=None)
-
-        x = keras.layers.Conv2D(3, (3, 3),
-                                activation='relu',
-                                padding='same',
-                                name='pre_conv')(input_layer)
-        outputs = base_model(x)
         # add end node
-        x = keras.layers.Flatten(name='flatten')(outputs[0])
+        x = keras.layers.Flatten(name='flatten')(base_model.outputs[0])
         x = keras.layers.Dense(4096, activation='relu', name='fc1')(x)
         x = keras.layers.Dense(4096, activation='relu', name='fc2')(x)
         x = keras.layers.Dense(classes, activation='softmax', name='predictions')(x)
         # create new model
-        self._model = keras.Model(input_layer, x, name='bci-vgg19')
+        self._model = keras.Model(input_tensor, x, name='bci-vgg19')
         self._model.summary()
+        self._model.compile(
+            optimizer=keras.optimizers.Adam(),
+            # preprocessing.LabelEncoder() is required
+            loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            metrics=["accuracy"]
+        )
 
     def predict(self, x):
         pass
@@ -79,4 +81,4 @@ class VGG19(ClassifierInterface):
 
 
 if __name__ == '__main__':
-    nn = VGG19(2, (64, 251, 2))
+    nn = VGG19(2, (64, 251))
