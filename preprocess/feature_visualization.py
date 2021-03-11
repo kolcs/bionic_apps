cbb0e61d0339cb5a8b994051de23043a7caf6868
Köllőd Csaba <kollod.csaba@itk.ppke.hu>
2021-03-11 15:30:00 +0100

    Feature visualization example code is done.
    - This is just a use case example
    - Both of the codes calculate the same morlet wavelet transformation but in different ways

--- preprocess/feature_visualization.py
+++ preprocess/feature_visualization.py
@@ -7,17 +7,17 @@ def extract_features(feature, db_name,
                      subject_list=None,
                      epoch_tmin=0, epoch_tmax=4,
                      window_length=1, window_step=.1,
+                     fast_load=True,
                      use_drop_subject_list=True,
                      filter_params=None):
     if filter_params is None:
         filter_params = {}
 
-    # generate database if not available
     proc = OfflineDataPreprocessor(
         init_base_config(),
         epoch_tmin=epoch_tmin, epoch_tmax=epoch_tmax,
         window_length=window_length, window_step=window_step,
-        fast_load=False, filter_params=filter_params,
+        fast_load=fast_load, filter_params=filter_params,
         use_drop_subject_list=use_drop_subject_list,
     )
     proc.use_db(db_name)
@@ -36,18 +36,49 @@ def show_mne_feature(proc, subject=None, task=None, epoch=0, window=0):
     epoch = proc.generate_mne_epoch(data)
     # now any kind of MNE feature extraction methods can be implemented and visualized
 
-    # example:
+    # Example:
+    # 1) feature generation. Can be done in feature_extraction.py also.
     from mne.time_frequency import tfr_morlet
     freqs = np.arange(2., 55.)
     n_cycles = freqs / 2.
-    power = tfr_morlet(epoch, freqs=freqs, n_cycles=n_cycles,
-                       return_itc=False, average=True)
-    power.plot(mode='mean',  # vmin=vmin, vmax=vmax,
-               title='Average of {} through all channels'.format(label), show=True)
-    for ch in power.ch_names:
-        p = power.copy().pick_channels([ch])
-        p.plot(mode='mean',  # vmin=vmin, vmax=vmax,
-               title='{} : channel {}'.format(label, ch), show=True)
+    wavelet = tfr_morlet(epoch, freqs=freqs, n_cycles=n_cycles,
+                         return_itc=False, average=True)
+    # 2) feature visualization
+    wavelet.plot(mode='mean',  # vmin=vmin, vmax=vmax,
+                 title='Average of {} through all channels'.format(label), show=True)
+    for ch in wavelet.ch_names:
+        ch_wavelet = wavelet.copy().pick_channels([ch])
+        ch_wavelet.plot(mode='mean',  # vmin=vmin, vmax=vmax,
+                        title='{} : channel {}'.format(label, ch), show=True)
+
+
+def show_numpy_feature(proc, subject=None, task=None, epoch=0, window=0):
+    if subject is None:
+        subject = 1
+    data, label = proc.get_feature(subject, task, epoch, window)
+    label = label[0]
+    # now any kind of feature extraction methods can be implemented and visualized
+
+    # Example:
+    # 1) feature generation. Can be done in feature_extraction.py also.
+    if len(data.shape) == 2:
+        data = np.expand_dims(data, 0)
+    from mne.time_frequency import tfr_array_morlet
+    freqs = np.arange(2., 55.)
+    n_cycles = freqs / 2.
+    wavelet = tfr_array_morlet(data, proc.fs, freqs, n_cycles, use_fft=False, output='power')
+    wavelet = wavelet[0, :, :, :]  # (n_epochs, n_chans, n_freqs, n_times)
+
+    # 2) feature visualization
+    from matplotlib import pyplot as plt
+    for i, ch in enumerate(proc.info['ch_names']):
+        plt.figure()
+        plt.imshow(wavelet[i, :, :], origin='lower', extent=[0, np.size(wavelet, -1) / proc.fs, 0, np.size(wavelet, 1)],
+                   aspect='auto')
+        plt.title('{} : channel {}'.format(label, ch))
+        plt.ylabel('frequencies')
+        plt.xlabel('time')
+        plt.show()
 
 
 if __name__ == '__main__':
@@ -57,15 +88,8 @@ if __name__ == '__main__':
     # mne feature visualization
     feature_extraction = dict(
         feature_type=FeatureType.RAW,
-        fft_low=7, fft_high=14
     )
     proc = extract_features(feature_extraction, db, subject_list=1, filter_params=filter_par)
-    show_mne_feature(proc)
 
-    # # numpy feature visualization
-    # feature_extraction = dict(
-    #     feature_type=FeatureType.FFT_POWER,
-    #     fft_low=7, fft_high=14
-    # )
-    # proc = extract_features(feature_extraction, db)
-    # show_feature(proc)
+    show_mne_feature(proc)
+    show_numpy_feature(proc)
