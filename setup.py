bd5397c4534e9c856c1887d18da0c0eb4d6919b8
Köllőd Csaba <kollod.csaba@itk.ppke.hu>
2021-05-25 13:14:58 +0200

    Package dependency installation

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Cybathlon dependencies install',
+    version='',
+    packages=[],
+    url='',
+    license='',
+    author='Köllőd Csaba',
+    author_email='kollod.csaba@itk.ppke.hu',
+    description='HUN BCISystem code for Cybathlon',
+    install_requires=['tensorflow', 'sklearn', 'pandas', 'matplotlib', 'pylsl', 'mne'],
+)
