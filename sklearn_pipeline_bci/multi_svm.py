d5e462f56e954ff31571eadcc1c908c5a1ace7ed
Köllőd Csaba <kollod.csaba@itk.ppke.hu>
2021-12-15 16:03:59 +0100

    SVM tests for statistical analysis.

--- sklearn_pipeline_bci/multi_svm.py
+++ sklearn_pipeline_bci/multi_svm.py
@@ -1,6 +1,10 @@
+from enum import Enum, auto
+
 import mne
 import numpy as np
 import pandas as pd
+from pathlib import Path
+from sklearn.decomposition import PCA
 from sklearn.ensemble import VotingClassifier
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.model_selection import StratifiedGroupKFold
@@ -8,7 +12,6 @@ from sklearn.pipeline import make_pipeline, FeatureUnion
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.preprocessing import LabelEncoder, StandardScaler, Normalizer, MinMaxScaler
 from sklearn.svm import SVC
-from sklearn.decomposition import PCA
 
 from preprocess import DataLoader, get_epochs_from_raw, SubjectHandle, standardize_channel_names, \
     Databases
@@ -94,15 +97,45 @@ def balance_epoch_nums(epochs, labels):
     return epochs[sel_ind], labels[sel_ind]
 
 
-def new_multi_svm(fs, fft_ranges):
+def get_fft_ranges_dict():
+    feat_par_list = {
+        'theta': dict(
+            feature_type=FeatureType.AVG_FFT_POWER,
+            fft_low=4, fft_high=7
+        ),
+        'alpha': dict(
+            feature_type=FeatureType.AVG_FFT_POWER,
+            fft_low=7, fft_high=14
+        ),
+        'beta': dict(
+            feature_type=FeatureType.AVG_FFT_POWER,
+            fft_low=14, fft_high=30
+        ),
+        'gamma': dict(
+            feature_type=FeatureType.AVG_FFT_POWER,
+            fft_low=30, fft_high=40
+        ),
+        'range30': dict(
+            feature_type=FeatureType.FFT_RANGE,
+            fft_low=4, fft_high=30
+        ),
+        'range40': dict(
+            feature_type=FeatureType.FFT_RANGE,
+            fft_low=2, fft_high=40
+        ),
+    }
+    return feat_par_list
+
+
+def new_multi_svm(fs, fft_ranges, *, method='psd2', norm=StandardScaler):
     inner_clfs = [(f'unit{i}', make_pipeline(AvgFFTCalc(fft_low, fft_high),
-                                             StandardScaler(), SVC()))
+                                             norm(), SVC(probability=True)))
                   for i, (fft_low, fft_high) in enumerate(fft_ranges)]
 
     clf = make_pipeline(
         FunctionTransformer(to_micro_volt),
-        FFTCalc(fs),
-        VotingClassifier(inner_clfs, n_jobs=len(inner_clfs)) if len(inner_clfs) > 1 else inner_clfs[0][1]
+        FFTCalc(fs, method),
+        VotingClassifier(inner_clfs, voting='soft', n_jobs=len(inner_clfs)) if len(inner_clfs) > 1 else inner_clfs[0][1]
     )
 
     return clf
@@ -126,12 +159,154 @@ def band_comb_svm(fs, fft_ranges):
         FunctionTransformer(to_micro_volt),
         FeatureUnion(parallel_lines),
         PCA(n_components=63),
-        SVC(cache_size=2048)
+        SVC(cache_size=2048, probability=True)
     )
     return clf
 
 
-def train_test_model(x, y, groups, fs, fft_ranges):
+def band_comb_multi_svm(fs, fft_ranges):
+    inner_clfs = [(f'unit{i}', band_comb_svm(fs, [fft_bin]))
+                  for i, fft_bin in enumerate(fft_ranges)]
+    assert len(inner_clfs) % 2 == 1
+    clf = make_pipeline(
+        # FunctionTransformer(to_micro_volt),
+        # FFTCalc(fs),
+        VotingClassifier(inner_clfs, voting='soft', n_jobs=len(inner_clfs)) if len(inner_clfs) > 1 else inner_clfs[0][1]
+    )
+
+    return clf
+
+
+def norm_test_svm(fs, *, norm=StandardScaler):
+    fft_ranges = []
+    for fft_par in get_fft_ranges_dict().values():
+        fft_ranges.extend(get_fft_ranges(**fft_par))
+    fft_ranges = sorted(set(fft_ranges), key=lambda tup: tup[0])
+
+    band_lines = FeatureUnion([(f'band {fft_low}-{fft_high}', AvgFFTCalc(fft_low, fft_high))
+                               for fft_low, fft_high in fft_ranges])
+
+    parallel_lines = [
+        make_pipeline(FFTCalc(fs, meth), band_lines, norm())
+        for meth in ['psd', 'psd2', 'fftabs', 'fftpow']
+    ]
+
+    parallel_lines = [(f'feature{i}', pl) for i, pl in enumerate(parallel_lines)]
+
+    clf = make_pipeline(
+        FunctionTransformer(to_micro_volt),
+        FeatureUnion(parallel_lines),
+        PCA(n_components=63),
+        SVC(cache_size=2048)
+    )
+
+    return clf
+
+
+def norm_test_multi_svm(fs, *, norm=StandardScaler):
+    fft_ranges = []
+    for fft_par in get_fft_ranges_dict().values():
+        fft_ranges.extend(get_fft_ranges(**fft_par))
+    fft_ranges = sorted(set(fft_ranges), key=lambda tup: tup[0])
+
+    def get_fft_bin(f_low, f_high):
+        fft_bins = [(method, make_pipeline(FFTCalc(fs, method), AvgFFTCalc(f_low, f_high)))
+                    for method in ['psd', 'psd2', 'fftabs', 'fftpow']]
+        return FeatureUnion(fft_bins)
+
+    inner_clfs = [(f'unit{i}', make_pipeline(get_fft_bin(fft_low, fft_high), norm(), SVC(probability=True)))
+                  for i, (fft_low, fft_high) in enumerate(fft_ranges)]
+
+    clf = make_pipeline(
+        FunctionTransformer(to_micro_volt),
+        VotingClassifier(inner_clfs, voting='soft', n_jobs=len(inner_clfs))
+        # if len(inner_clfs) > 1 else inner_clfs[0][1]
+    )
+
+    return clf
+
+
+def fft_test_svm(fs, method='psd2'):
+    fft_ranges = []
+    for fft_par in get_fft_ranges_dict().values():
+        fft_ranges.extend(get_fft_ranges(**fft_par))
+    fft_ranges = sorted(set(fft_ranges), key=lambda tup: tup[0])
+
+    normalizers = FeatureUnion([(norm.__name__, norm()) for norm in
+                                [FunctionTransformer, Normalizer, MinMaxScaler, StandardScaler]])
+
+    band_lines = FeatureUnion([(f'band {fft_low}-{fft_high}',
+                                make_pipeline(AvgFFTCalc(fft_low, fft_high), normalizers))
+                               for fft_low, fft_high in fft_ranges])
+
+    clf = make_pipeline(
+        FunctionTransformer(to_micro_volt),
+        FFTCalc(fs, method),
+        band_lines,
+        PCA(n_components=63),
+        SVC(cache_size=2048)
+    )
+
+    return clf
+
+
+def fft_test_multi_svm(fs, method='psd2'):
+    fft_ranges = []
+    for fft_par in get_fft_ranges_dict().values():
+        fft_ranges.extend(get_fft_ranges(**fft_par))
+    fft_ranges = sorted(set(fft_ranges), key=lambda tup: tup[0])
+
+    normalizers = FeatureUnion([(norm.__name__, norm()) for norm in
+                                [FunctionTransformer, Normalizer, MinMaxScaler, StandardScaler]])
+
+    inner_clfs = [(f'unit{i}', make_pipeline(
+        AvgFFTCalc(fft_low, fft_high), normalizers,
+        PCA(n_components=63), SVC(probability=True))
+                   ) for i, (fft_low, fft_high) in enumerate(fft_ranges)]
+
+    clf = make_pipeline(
+        FunctionTransformer(to_micro_volt),
+        FFTCalc(fs, method),
+        VotingClassifier(inner_clfs, voting='soft', n_jobs=len(inner_clfs))
+        # if len(inner_clfs) > 1 else inner_clfs[0][1]
+    )
+
+    return clf
+
+
+class TestType(Enum):
+    NO_TEST = auto()
+    BAND_COMB = auto()
+    NORM_SINGLE = auto()
+    NORM_MULTI = auto()
+    FFT_SINGLE = auto()
+    FFT_MULTI = auto()
+
+
+TEST = TestType.NO_TEST
+
+
+def get_classifier(fs, fft_ranges, norm, method):
+    if TEST is TestType.NO_TEST:
+        clf = new_multi_svm(fs, fft_ranges, method=method, norm=norm)
+    elif TEST is TestType.BAND_COMB:
+        clf = band_comb_multi_svm(fs, fft_ranges)
+    elif TEST is TestType.NORM_SINGLE:
+        clf = norm_test_svm(fs, norm=norm)
+    elif TEST is TestType.NORM_MULTI:
+        clf = norm_test_multi_svm(fs, norm=norm)
+    elif TEST is TestType.FFT_SINGLE:
+        clf = fft_test_svm(fs, method)
+    elif TEST is TestType.FFT_MULTI:
+        clf = fft_test_multi_svm(fs, method)
+
+    else:
+        raise NotImplementedError
+
+    return clf
+
+
+def train_test_model(x, y, groups, fs, fft_ranges, norm=StandardScaler, method='psd2'):
     label_encoder = LabelEncoder()
     y = label_encoder.fit_transform(y)
     kfold = StratifiedGroupKFold(shuffle=False)
@@ -143,11 +318,10 @@ def train_test_model(x, y, groups, fs, fft_ranges):
         test_x = x[test]
         test_y = y[test]
 
-        # clf = new_multi_svm(fs, fft_ranges)
-        clf = band_comb_svm(fs, fft_ranges)
-
+        clf = get_classifier(fs, fft_ranges, norm, method)
         clf.fit(train_x, train_y)
         y_pred = clf.predict(test_x)
+
         y_pred = label_encoder.inverse_transform(y_pred)
         test_y = label_encoder.inverse_transform(test_y)
 
@@ -172,6 +346,8 @@ def test_db(feature_params, db_name,
             filter_params=None,
             do_artefact_rejection=True,
             balance_data=True,
+            norm=StandardScaler,
+            method='psd2',
             log_file='out.csv'):
     if filter_params is None:
         filter_params = {}
@@ -232,7 +408,7 @@ def test_db(feature_params, db_name,
         fft_ranges = get_fft_ranges(**feature_params)
 
         print("####### Classification report for subject{}: #######".format(subj))
-        cross_acc = train_test_model(features, labels, groups, fs, fft_ranges)
+        cross_acc = train_test_model(features, labels, groups, fs, fft_ranges, norm, method)
         res['Subject'].append(subj)
         res['Accuracy list'].append(cross_acc)
         res['Std of Avg. Acc'].append(np.std(cross_acc))
@@ -240,33 +416,106 @@ def test_db(feature_params, db_name,
         pd.DataFrame(res).to_csv(log_file, sep=';', encoding='utf-8', index=False)
 
 
-def run_new_multi_svm():
-    test_db(
-        feature_params=dict(
-            feature_type=FeatureType.AVG_FFT_POWER,
-            fft_low=4, fft_high=7
-        ),
-        db_name=Databases.PHYSIONET,
-        filter_params=dict(  # required for FASTER artefact filter
-            order=5, l_freq=1, h_freq=45
-        ),
-        do_artefact_rejection=True,
-        log_file='theta_svm.csv'
-    )
+def multi_svm_test(db_name):
+    path = Path(db_name.name, 'multi')
+    path.mkdir(parents=True, exist_ok=True)
 
-    test_db(
-        feature_params=dict(
-            feature_type=FeatureType.AVG_FFT_POWER,
-            fft_low=30, fft_high=45
-        ),
-        db_name=Databases.PHYSIONET,
-        filter_params=dict(  # required for FASTER artefact filter
-            order=5, l_freq=1, h_freq=45
-        ),
-        do_artefact_rejection=True,
-        log_file='gamma_svm.csv'
-    )
+    for band, feature_params in get_fft_ranges_dict().items():
+        test_db(
+            feature_params=feature_params,
+            db_name=db_name,
+            filter_params=dict(  # required for FASTER artefact filter
+                order=5, l_freq=1, h_freq=45
+            ),
+            do_artefact_rejection=True,
+            log_file=str(path.joinpath(band + '_multi_svm.csv'))
+        )
+
+
+def band_comb_test(db_name):
+    global TEST
+    TEST = TestType.BAND_COMB
+
+    path = Path(db_name.name, 'band')
+    path.mkdir(parents=True, exist_ok=True)
+
+    for band, feature_params in get_fft_ranges_dict().items():
+        test_db(
+            feature_params=feature_params,
+            db_name=db_name,
+            filter_params=dict(  # required for FASTER artefact filter
+                order=5, l_freq=1, h_freq=45
+            ),
+            do_artefact_rejection=True,
+            log_file=str(path.joinpath(band + '_band_svm.csv'))
+        )
+
+
+def norm_test(db_name):
+    global TEST
+
+    path = Path(db_name.name, 'norm')
+    path.mkdir(parents=True, exist_ok=True)
+
+    for TEST in [TestType.NORM_SINGLE, TestType.NORM_MULTI]:
+
+        for norm in [FunctionTransformer, Normalizer, StandardScaler, MinMaxScaler]:
+            if norm is FunctionTransformer:
+                log_file = 'no norm'
+            elif norm is Normalizer:
+                log_file = 'l2 norm'
+            elif norm is StandardScaler:
+                log_file = 'Standard Scale'
+            elif norm is MinMaxScaler:
+                log_file = 'MinMax Scale'
+            else:
+                raise ValueError
+
+            test_db(
+                feature_params=dict(  # this param is ignored but required
+                    feature_type=FeatureType.AVG_FFT_POWER,
+                    fft_low=14, fft_high=30
+                ),
+                db_name=db_name,
+                filter_params=dict(  # required for FASTER artefact filter
+                    order=5, l_freq=1, h_freq=45
+                ),
+                do_artefact_rejection=True,
+                norm=norm,
+                log_file=str(path.joinpath(f'{log_file}_{TEST.name}_svm.csv'))
+            )
+
+
+def fft_test(db_name):
+    global TEST
+
+    path = Path(db_name.name, 'base_feature')
+    path.mkdir(parents=True, exist_ok=True)
+
+    for TEST in [TestType.FFT_SINGLE, TestType.FFT_MULTI]:
+
+        for method in ['psd', 'psd2', 'fftabs', 'fftpow']:
+            test_db(
+                feature_params=dict(  # this param is ignored but required
+                    feature_type=FeatureType.AVG_FFT_POWER,
+                    fft_low=14, fft_high=30
+                ),
+                db_name=db_name,
+                filter_params=dict(  # required for FASTER artefact filter
+                    order=5, l_freq=1, h_freq=45
+                ),
+                do_artefact_rejection=True,
+                method=method,
+                log_file=str(path.joinpath(f'{method}_{TEST.name}_svm.csv'))
+            )
+
+
+def make_tests_on(db_name=Databases.PHYSIONET):
+    multi_svm_test(db_name)
+    band_comb_test(db_name)
+    norm_test(db_name)
+    fft_test(db_name)
 
 
 if __name__ == '__main__':
-    run_new_multi_svm()
+    make_tests_on()
