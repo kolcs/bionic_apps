e49b1ac71e2fbe0cf016a57fabee45a291ce4d89
Köllőd Csaba <kollod.csaba@itk.ppke.hu>
2022-06-08 22:46:43 +0200

    BrainDriver control under dev

--- bionic_apps/games/braindriver/main_game_start.py
+++ bionic_apps/games/braindriver/main_game_start.py
@@ -1,45 +1,66 @@
-from gui_handler import select_files_in_explorer
+from ...databases import get_eeg_db_name_by_filename, EEG_Databases
+from ...feature_extraction import FeatureType
+from ...handlers.gui import select_files_in_explorer
+from ...handlers.hdf5 import HDF5Dataset, init_hdf5_db
+from ...preprocess.dataset_generation import generate_subject_data
+from ...preprocess.io import DataLoader
+from ...utils import init_base_config
+from ...offline_analyses import train_test_data
 
-from BCISystem import BCISystem, FeatureType
-from paramsel import parallel_search_for_fft_params
-from preprocess import init_base_config, get_db_name_by_filename
+CMD_IN = 1.5  # sec
+DB_FILENAME = 'brain_driver_db.hdf5'
+SUBJ = 0
 
-CMD_IN = 1.6  # sec
-
-FFT_POWERS = [(22, 30), (24, 40), (24, 34)]
-
-# svm params:
-C, GAMMA = 309.27089776753826, 0.3020223611011116
+# FFT_POWERS = [(22, 30), (24, 40), (24, 34)]
+#
+# # svm params:
+# C, GAMMA = 309.27089776753826, 0.3020223611011116
 
 if __name__ == '__main__':
     print('Starting BCI System for game play...')
 
-    eeg_file = select_files_in_explorer(init_base_config(),
-                                        message='Select EEG file for training the BCI System!')
-    assert len(eeg_file) == 1, 'Please select only one EEG file!'
-    eeg_file = eeg_file[0]
-    db_name = get_db_name_by_filename(eeg_file)
+    eeg_files = select_files_in_explorer(init_base_config(),
+                                         message='Select EEG file for training the BCI System!')
+    assert len(eeg_files) == 1, 'Please select only one EEG file!'
+    eeg_files = eeg_files[0]
+    db_name = get_eeg_db_name_by_filename(eeg_files)
 
-    classifier_kwargs = dict(C=C, gamma=GAMMA)
-
-    # fft_ranges = FFT_POWERS
-    fft_ranges = parallel_search_for_fft_params(
-        eeg_file, db_name,
-        fft_search_min=14,
-        fft_search_max=40,
-        fft_search_step=2,
-        best_n_fft=7,
-        classifier_kwargs=classifier_kwargs
+    filter_params = dict(
+        order=5, l_freq=1, h_freq=45
     )
 
-    print('Selected fft params:')
-    print(fft_ranges)
+    # classifier_kwargs = dict(C=C, gamma=GAMMA)
 
-    feature_extraction = dict(
-        feature_type=FeatureType.MULTI_AVG_FFT_POW,
-        # fft_low=14, fft_high=30, fft_step=2, fft_width=2,
-        fft_ranges=fft_ranges
+    loader = DataLoader()
+    loader.use_db(db_name)
+
+    feature_params = dict(
+        feature_type=FeatureType.FFT_RANGE,
+        feature_kwargs=dict(fft_low=2, fft_high=40),
+        filter_params=filter_params,
+        epoch_tmin=0, epoch_tmax=4,
+        window_length=2, window_step=.1,
+        do_artefact_rejection=True, balance_data=True,
+        binarize_labels=db_name is EEG_Databases.GAME_PAR_D
     )
+    database = HDF5Dataset(DB_FILENAME, feature_params)
+
+    if not database.exists():
+        subj_data = generate_subject_data(
+            eeg_files, loader, SUBJ, **feature_params
+        )
+        database.add_data(*subj_data)
+    database.close()
+
+    db, y_all, subj_ind, ep_ind, le = init_hdf5_db(DB_FILENAME)
+
+    x = db.get_data(subj_ind == SUBJ)
+    y = y_all[subj_ind == SUBJ]
+    groups = ep_ind[subj_ind == SUBJ]
+
+    # todo: cross-train / select best
+    cross_acc = train_test_data(classifier_type, x, y, groups=groups, lab_enc=le,
+                                n_splits=n_splits, shuffle=True, **classifier_kwargs)
 
     bci = BCISystem()
     bci.play_game(
@@ -50,7 +71,7 @@ if __name__ == '__main__':
         epoch_tmax=4,
         command_delay=CMD_IN,
         use_binary_game_logger=True,
-        train_file=eeg_file,
+        train_file=eeg_files,
         classifier_kwargs=classifier_kwargs,
         make_opponents=True
     )
--- bionic_apps/preprocess/dataset_generation.py
+++ bionic_apps/preprocess/dataset_generation.py
@@ -10,6 +10,62 @@ from ..utils import standardize_eeg_channel_names, filter_mne_obj, balance_epoch
     window_epochs
 
 
+def generate_subject_data(files, loader, subj, filter_params,
+                          epoch_tmin, epoch_tmax, window_length, window_step,
+                          feature_type, feature_kwargs=None,
+                          do_artefact_rejection=True, balance_data=True,
+                          binarize_labels=False):
+    if feature_kwargs is None:
+        feature_kwargs = {}
+
+    task_dict = loader.get_task_dict()
+    event_id = loader.get_event_id()
+    print(f'\nSubject{subj}')
+    raws = [mne.io.read_raw(file) for file in files]
+    raw = mne.io.concatenate_raws(raws)
+    raw.load_data()
+    fs = raw.info['sfreq']
+
+    standardize_eeg_channel_names(raw)
+    try:  # check available channel positions
+        mne.channels.make_eeg_layout(raw.info)
+    except RuntimeError:  # if no channel positions are available create them from standard positions
+        montage = mne.channels.make_standard_montage('standard_1005')  # 'standard_1020'
+        raw.set_montage(montage, on_missing='warn')
+
+    if len(filter_params) > 0:
+        raw = filter_mne_obj(raw, **filter_params)
+
+    epochs = get_epochs_from_raw(raw, task_dict,
+                                 epoch_tmin=epoch_tmin, epoch_tmax=epoch_tmax,
+                                 event_id=event_id)
+    del raw
+
+    if do_artefact_rejection:
+        epochs = ArtefactFilter(apply_frequency_filter=False).offline_filter(epochs)
+
+    ep_labels = [list(epochs[i].event_id)[0] for i in range(len(epochs))]
+
+    if balance_data:
+        epochs, ep_labels = balance_epoch_nums(epochs, ep_labels)
+
+    if binarize_labels:
+        ep_labels = [_create_binary_label(label) for label in ep_labels]
+
+    # window the epochs
+    windowed_data = window_epochs(epochs.get_data(),
+                                  window_length=window_length, window_step=window_step,
+                                  fs=fs)
+    del epochs
+
+    groups = [i // windowed_data.shape[1] for i in range(windowed_data.shape[0] * windowed_data.shape[1])]
+    labels = [ep_labels[i // windowed_data.shape[1]] for i in range(len(groups))]
+    windowed_data = np.vstack(windowed_data)
+
+    windowed_data = generate_features(windowed_data, fs, feature_type, **feature_kwargs)
+    return windowed_data, labels, [subj] * len(labels), groups
+
+
 def generate_eeg_db(db_name, db_filename, feature_type=FeatureType.RAW,
                     epoch_tmin=0, epoch_tmax=4,
                     window_length=2, window_step=.1,
@@ -39,50 +95,12 @@ def generate_eeg_db(db_name, db_filename, feature_type=FeatureType.RAW,
     if not (database.exists() and fast_load):
         for subj in loader.get_subject_list():
             files = loader.get_filenames_for_subject(subj)
-            task_dict = loader.get_task_dict()
-            event_id = loader.get_event_id()
-            print(f'\nSubject{subj}')
-            raws = [mne.io.read_raw(file) for file in files]
-            raw = mne.io.concatenate_raws(raws)
-            raw.load_data()
-            fs = raw.info['sfreq']
-
-            standardize_eeg_channel_names(raw)
-            try:  # check available channel positions
-                mne.channels.make_eeg_layout(raw.info)
-            except RuntimeError:  # if no channel positions are available create them from standard positions
-                montage = mne.channels.make_standard_montage('standard_1005')  # 'standard_1020'
-                raw.set_montage(montage, on_missing='warn')
-
-            if len(filter_params) > 0:
-                raw = filter_mne_obj(raw, **filter_params)
-
-            epochs = get_epochs_from_raw(raw, task_dict,
-                                         epoch_tmin=epoch_tmin, epoch_tmax=epoch_tmax,
-                                         event_id=event_id)
-            del raw
-
-            if do_artefact_rejection:
-                epochs = ArtefactFilter(apply_frequency_filter=False).offline_filter(epochs)
-
-            ep_labels = [list(epochs[i].event_id)[0] for i in range(len(epochs))]
-
-            if balance_data:
-                epochs, ep_labels = balance_epoch_nums(epochs, ep_labels)
-
-            if db_name is EEG_Databases.GAME_PAR_D:
-                ep_labels = [_create_binary_label(label) for label in ep_labels]
-
-            # window the epochs
-            windowed_data = window_epochs(epochs.get_data(),
-                                          window_length=window_length, window_step=window_step,
-                                          fs=fs)
-            del epochs
-
-            groups = [i // windowed_data.shape[1] for i in range(windowed_data.shape[0] * windowed_data.shape[1])]
-            labels = [ep_labels[i // windowed_data.shape[1]] for i in range(len(groups))]
-            windowed_data = np.vstack(windowed_data)
-
-            windowed_data = generate_features(windowed_data, fs, feature_type, **feature_kwargs)
-            database.add_data(windowed_data, labels, [subj] * len(labels), groups)
+            subj_data = generate_subject_data(
+                files, loader, subj, filter_params,
+                epoch_tmin, epoch_tmax, window_length, window_step,
+                feature_type, feature_kwargs,
+                do_artefact_rejection, balance_data,
+                binarize_labels=db_name is EEG_Databases.GAME_PAR_D
+            )
+            database.add_data(*subj_data)
         database.close()
