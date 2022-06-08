af2305da204d8234ee8db3dbd7b24e4854958f4f
Köllőd Csaba <kollod.csaba@itk.ppke.hu>
2022-06-08 17:01:59 +0200

    Example: custom_feature_classifier.py

---
+++ examples/custom_feature_classifier.py
@@ -0,0 +1,33 @@
+from sklearn.ensemble import ExtraTreesClassifier
+
+from bionic_apps.ai import ClassifierType
+from bionic_apps.databases import EEG_Databases
+from bionic_apps.feature_extraction import FeatureType, get_hugines_transfromer
+from bionic_apps.offline_analyses import test_eegdb_within_subject
+
+feature_type = FeatureType.USER_PIPELINE
+classifier_type = ClassifierType.USER_DEFINED
+
+db_name = EEG_Databases.PHYSIONET
+fs = 160
+chs = 64
+
+filter_params = dict(
+    order=5, l_freq=1, h_freq=45
+)
+
+feature_kwargs = dict(
+    pipeline=get_hugines_transfromer()
+)
+
+classifier_kwargs = dict(
+    classifier=ExtraTreesClassifier(n_estimators=250, n_jobs=-2)
+)
+
+test_eegdb_within_subject(db_name, feature_type,
+                          filter_params=filter_params,
+                          feature_kwargs=feature_kwargs,
+                          classifier_type=classifier_type,
+                          classifier_kwargs=classifier_kwargs,
+                          do_artefact_rejection=True, fast_load=True,
+                          log_file='out.csv')
