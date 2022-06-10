9cf31805e1089fbb665db95062049f8bb8bb4bc5
Köllőd Csaba <kollod.csaba@itk.ppke.hu>
2022-06-10 13:55:35 +0200

    Update: dependency install

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup
 
+# todo: Conda install liblsl
+#  conda install -c conda-forge liblsl
+#  https://niteo.co/blog/setuptools-run-custom-code-in-setup-py
+#  https://stackoverflow.com/questions/36539623/how-do-i-find-the-name-of-the-conda-environment-in-which-my-code-is-running
+
 setup(
-    name='Cybathlon dependencies install',
+    name='bionic-apps',
     version='',
     packages=[],
     url='',
     license='',
     author='Köllőd Csaba',
     author_email='kollod.csaba@itk.ppke.hu',
-    description='HUN BCISystem code for Cybathlon',
+    description='Bionic Applications dependencies install',
     install_requires=['tensorflow', 'scikit-learn', 'pandas', 'matplotlib', 'pylsl', 'mne', 'numpy', 'joblib',
-                      'tblib'],
+                      'h5py', 'scipy'],
 )
