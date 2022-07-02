3deca7bb06ed90b436b5d132c98a37e60f5e502a
Köllőd Csaba <kollod.csaba@itk.ppke.hu>
2022-07-02 15:53:23 +0200

    Update: parallel db creation
    - threshold for parallel db generation
    - sequential db generation is better for old computers or computers with few cpu cores

--- bionic_apps/preprocess/dataset_generation.py
+++ bionic_apps/preprocess/dataset_generation.py
@@ -4,6 +4,7 @@ from time import time
 import mne
 import numpy as np
 from joblib import Parallel, delayed
+from psutil import cpu_count
 
 from .io import DataLoader, SubjectHandle, get_epochs_from_raw
 from ..artifact_filtering.faster import ArtefactFilter
@@ -12,6 +13,8 @@ from ..feature_extraction import FeatureType, generate_features
 from ..handlers.hdf5 import HDF5Dataset
 from ..utils import standardize_eeg_channel_names, filter_mne_obj, balance_epoch_nums, _create_binary_label, \
     window_epochs
+
+CPU_THRESHOLD = 10
 
 
 def generate_subject_data(files, loader, subj, filter_params,
@@ -111,7 +114,7 @@ def generate_eeg_db(db_name, db_filename, feature_type=FeatureType.RAW,
                     balance_data=True,
                     subject_handle=SubjectHandle.INDEPENDENT_DAYS,
                     base_dir='.', fast_load=True,
-                    n_subjects='all', n_jobs=-3):
+                    n_subjects='all', n_jobs=-2):
     if filter_params is None:
         filter_params = {}
     if feature_kwargs is None:
@@ -139,26 +142,27 @@ def generate_eeg_db(db_name, db_filename, feature_type=FeatureType.RAW,
             subject_list = loader.get_subject_list()[:n_subjects]
 
         tic = time()
-        # for subj in subject_list:
-        #     files = loader.get_filenames_for_subject(subj)
-        #     artifact_filter = ArtefactFilter(apply_frequency_filter=False) if do_artefact_rejection else None
-        #     windowed_data, labels, subj_ind, ep_ind, fs, info = generate_subject_data(
-        #         files, loader, subj, filter_params,
-        #         epoch_tmin, epoch_tmax, window_length, window_step,
-        #         artifact_filter, balance_data,
-        #         binarize_labels=db_name is EEG_Databases.GAME_PAR_D
-        #     )
-        #     windowed_data = generate_features(windowed_data, fs, feature_type, info=info, **feature_kwargs)
-        #     database.add_data(windowed_data, labels, subj_ind, ep_ind, fs)
-
-        subj_db_files = Parallel(n_jobs)(
-            delayed(_save_one_subject_data)(feature_type, feature_kwargs, loader, subj,
-                                            epoch_tmin, epoch_tmax, window_length, window_step,
-                                            filter_params, balance_data,
-                                            db_name is EEG_Databases.GAME_PAR_D,
-                                            do_artefact_rejection,
-                                            db_filename.parent) for subj in subject_list)
-        _merge_database(database, subj_db_files)
+        if cpu_count() < CPU_THRESHOLD:  # parallel db generation is slow if there is not enough cpu_cores
+            for subj in subject_list:
+                files = loader.get_filenames_for_subject(subj)
+                artifact_filter = ArtefactFilter(apply_frequency_filter=False) if do_artefact_rejection else None
+                windowed_data, labels, subj_ind, ep_ind, fs, info = generate_subject_data(
+                    files, loader, subj, filter_params,
+                    epoch_tmin, epoch_tmax, window_length, window_step,
+                    artifact_filter, balance_data,
+                    binarize_labels=db_name is EEG_Databases.GAME_PAR_D
+                )
+                windowed_data = generate_features(windowed_data, fs, feature_type, info=info, **feature_kwargs)
+                database.add_data(windowed_data, labels, subj_ind, ep_ind, fs)
+        else:
+            subj_db_files = Parallel(n_jobs)(
+                delayed(_save_one_subject_data)(feature_type, feature_kwargs, loader, subj,
+                                                epoch_tmin, epoch_tmax, window_length, window_step,
+                                                filter_params, balance_data,
+                                                db_name is EEG_Databases.GAME_PAR_D,
+                                                do_artefact_rejection,
+                                                db_filename.parent) for subj in subject_list)
+            _merge_database(database, subj_db_files)
 
         database.close()
         print(f'DB generated under {(time() - tic) / 60:.2f} minutes')
