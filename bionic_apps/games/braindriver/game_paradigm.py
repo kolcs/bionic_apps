b015e811d0de661a5ab092a25e957929851c8d83
Köllőd Csaba <kollod.csaba@itk.ppke.hu>
2022-10-18 11:50:22 +0200

    Update: game paradigm

--- bionic_apps/games/braindriver/game_paradigm.py
+++ bionic_apps/games/braindriver/game_paradigm.py
@@ -1,8 +1,10 @@
 from multiprocessing import Pipe
 
 from bionic_apps.databases import Databases
+from bionic_apps.databases.eeg.standardize_database import convert_game_paradigm
 from bionic_apps.games.braindriver.logger import GameLogger
 from bionic_apps.games.braindriver.opponents import MasterPlayer
+from bionic_apps.handlers import show_message
 from bionic_apps.preprocess.io import DataLoader
 
 
@@ -10,20 +12,22 @@ def run_braindriver_paradigm(db_name=Databases.PILOT_PAR_B):
     loader = DataLoader()
     loader.use_db(db_name)
 
-    # from bionic_apps.external_connections.brainvision import RemoteControlClient
     # rcc = RemoteControlClient(print_received_messages=False)
     # rcc.open_recorder()
-    # rcc.view_data()
+    # rcc.check_impedance()
+
     print('Connecting to game...')
     parent_conn, child_conn = Pipe()
-
     game_log = GameLogger(data_loader=loader, connection=child_conn, annotator='bv_rcc')
-    # game_log = GameLogger(data_loader=loader, connection=child_conn, annotator=None)
     game_log.start()
     MasterPlayer(player_num=1, game_log_conn=parent_conn, reaction_time=.5).start()
 
-    while True:
-        pass
+    show_message(
+        'Press OK only, if you finished with the paradigm!!!'
+    )
+    # del rcc
+
+    convert_game_paradigm()
 
 
 if __name__ == '__main__':
